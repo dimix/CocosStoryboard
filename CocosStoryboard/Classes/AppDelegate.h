7f5c2df4e8f3cf445cdcf6301113729e29b96e0c
dimix <dimitri.giani@gmail.com>
2014-07-29 01:08:05 +0200

    Initial Commit

---
+++ CocosStoryboard/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CocosStoryboard
+//
+//  Created by Dimitri Giani on 29/07/14.
+//  Copyright Dimitri Giani 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CocosStoryboard/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocosStoryboard
+//
+//  Created by Dimitri Giani on 29/07/14.
+//  Copyright Dimitri Giani 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
