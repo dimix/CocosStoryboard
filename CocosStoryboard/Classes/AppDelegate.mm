4c0dd8a2bc8420f4c7a6d087ca8aeba84861e6c4
dimix <dimitri.giani@gmail.com>
2014-08-13 11:48:40 +0200

    Updated AppDelegate removing unused code.

--- CocosStoryboard/Classes/AppDelegate.mm
+++ CocosStoryboard/Classes/AppDelegate.mm
@@ -8,51 +8,21 @@
 // -----------------------------------------------------------------------
 
 #import "AppDelegate.h"
-#import "IntroScene.h"
-//#import "HelloWorldScene.h"
-
 #import "CCBReader.h"
-#import "CocosViewController.h"
 
-@interface AppDelegate() <CocosViewControllerDelegate>
+@interface AppDelegate()
 
 @end
 
 @implementation AppDelegate
 
--(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 	// Configure CCFileUtils to work with SpriteBuilder
 	
     [CCBReader configureCCFileUtils];
 	
-	//	// Configure Cocos2d with the options set in SpriteBuilder
-	//    NSString* configPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"]; // TODO: add support for Published-Android support
-	//    configPath = [configPath stringByAppendingPathComponent:@"configCocos2d.plist"];
-	//
-	//    NSMutableDictionary* cocos2dSetup = [NSMutableDictionary dictionaryWithContentsOfFile:configPath];
-	//
-	//    // Note: this needs to happen before configureCCFileUtils is called, because we need apportable to correctly setup the screen scale factor.
-	//
-	//#ifdef APPORTABLE
-	//    if([cocos2dSetup[CCSetupScreenMode] isEqual:CCScreenModeFixed])
-	//        [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenAspectFitEmulationMode];
-	//    else
-	//        [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenScaledAspectFitEmulationMode];
-	//#endif
-	//
-	//	CCViewController* controller = (CCViewController*)self.window.rootViewController;
-	//
-	//	controller.delegate = self;
-	//	controller.ccConfig = cocos2dSetup;
-	
 	return YES;
-}
-
-- (CCScene *)cocosViewControllerSceneToRun:(CocosViewController *)cocosViewController
-{
-	//	return [IntroScene scene];
-	return [CCBReader loadAsScene:@"MainScenePhone"];
 }
 
 @end
